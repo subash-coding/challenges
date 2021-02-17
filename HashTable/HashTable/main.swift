7941cf7de0590dfb0e82a9e87cfe55cf24adcacf
subash <thapa.subash345@gmail.com>
2021-02-16 22:02:53 -0800

    implemented hash table

--- HashTable/HashTable/main.swift
+++ HashTable/HashTable/main.swift
@@ -7,5 +7,51 @@
 
 import Foundation
 
-print("Hello, World!")
+public struct HashTable<Key: Hashable, Value> {
+    private typealias Element = (key: Key, value: Value)
+    private typealias Bucket = [Element]
+    private var buckets: [Bucket]
+    
+    private(set) var count = 0
+    public var isEmpty: Bool {
+        return count == 0
+    }
+    
+    public init(capacity: Int) {
+        assert(capacity>0)
+        buckets = Array<Bucket>(repeating: [], count: capacity)
+    }
+    
+    private func index(for key: Key) -> Int {
+        return abs(key.hashValue) % buckets.count
+    }
+    
+    public subscript(key: Key) -> Value? {
+        get{
+            let index = self.index(for: key)
+            return buckets[index].first { $0.key == key}?.value
+        }
+        set {
+            if let value = newValue {
+                update(value: value, for: key)
+            }
+        }
+    }
+    
+    @discardableResult
+    public mutating func update(value: Value, for key: Key) -> Value? {
+        let index = self.index(for: key)
+        
+        if let (i, element) = buckets[index].enumerated().first(where: { $0.1.key == key }) {
+          let oldValue = element.value
+          buckets[index][i].value = value
+          return oldValue
+        }
+        
+        buckets[index].append((key: key, value: value))
+        count += 1
+        return nil
+    }
+    
+}
 
