a4816aeac3643af96da8cbd803067846509a9f11
subash <thapa.subash345@gmail.com>
2021-02-17 23:33:13 -0800

    cleanup

--- HashTable/HashTable/main.swift
+++ HashTable/HashTable/main.swift
@@ -7,53 +7,7 @@
 
 import Foundation
 
-public struct MyHashTable<Key: Hashable, Value> {
-    private typealias Element = (key: Key, value: Value)
-    private typealias Bucket = [Element]
-    private var buckets: [Bucket]
-    
-    private(set) var count = 0
-    public var isEmpty: Bool {
-        return count == 0
-    }
-    
-    public init(capacity: Int) {
-        assert(capacity>0)
-        buckets = Array<Bucket>(repeating: [], count: capacity)
-    }
-    
-    private func index(for key: Key) -> Int {
-        return abs(key.hashValue) % buckets.count
-    }
-    
-    public subscript(key: Key) -> Value? {
-        get{
-            let index = self.index(for: key)
-            return buckets[index].first { $0.key == key}?.value
-        }
-        set {
-            if let value = newValue {
-                update(value: value, for: key)
-            }
-        }
-    }
-    
-    @discardableResult
-    public mutating func update(value: Value, for key: Key) -> Value? {
-        let index = self.index(for: key)
-        
-        if let (i, element) = buckets[index].enumerated().first(where: { $0.1.key == key }) {
-          let oldValue = element.value
-          buckets[index][i].value = value
-          return oldValue
-        }
-        
-        buckets[index].append((key: key, value: value))
-        count += 1
-        return nil
-    }
-    
-}
+//######### Custom Hashtable Testing #########################################
 
 var hashTable = MyHashTable<String,String>(capacity: 5)
 hashTable["firstname"] = "Subash"
